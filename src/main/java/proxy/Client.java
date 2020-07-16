b4fb733b1b12cc9618bdc4e409cc31e843f8b0ae
Konstantin Pinegin <pineginkv@gmail.com>
2020-07-17 03:41:04 +0800

    works but simplistic

--- src/main/java/proxy/Client.java
+++ src/main/java/proxy/Client.java
@@ -5,15 +5,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Client {
-    public InputStream inputStream;
+    private InputStream inputStream;
+    private Map<String, String> responseHeaders;
 
     public void start() {
         try {
             URL url = new URL("http://fluxfm.hoerradar.de/fluxfm-berlin");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
+            setHeaders(connection);
             this.inputStream = connection.getInputStream();
             System.out.println("passed through GET");
 //            while (true) {
@@ -24,6 +28,30 @@ public class Client {
             //connection.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public InputStream getInputStream() {
+        if (inputStream == null){
+            start();
+        }
+        return inputStream;
+    }
+
+    public Map<String, String> getResponseHeaders() {
+        if (responseHeaders == null){
+            start();
+        }
+        return responseHeaders;
+    }
+    private void setHeaders (HttpURLConnection connection) {
+        responseHeaders = new HashMap<>();
+        int i = 1;
+        while (connection.getHeaderField(i)!=null){
+            String field = connection.getHeaderField(i);
+            String key = connection.getHeaderFieldKey(i);
+            responseHeaders.put(field, key);
+            i++;
         }
     }
 }
--- src/main/java/proxy/MyHttpHandler.java
+++ src/main/java/proxy/MyHttpHandler.java
@@ -6,11 +6,16 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.text.StringEscapeUtils;
 
 import java.io.*;
+import java.util.Map;
 
-public class MyHttpHandler implements  HttpHandler {
+public class MyHttpHandler implements HttpHandler {
     private Client client;
+    public MyHttpHandler(){
+    }
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        System.out.println("handle");
 
         String requestParamValue;
         requestParamValue = handleGetRequest(httpExchange);
@@ -23,39 +28,36 @@ public class MyHttpHandler implements  HttpHandler {
     }
 
     private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
-        if (client == null){
+        if (client == null) {
             client = new Client();
-            new Thread(()-> client.start()).start();
+//            new Thread(() -> client.start()).start();
         }
+
         OutputStream outputStream = httpExchange.getResponseBody();
-        httpExchange.getResponseHeaders().set("Content-Type","audio/mpeg");
-        httpExchange.getResponseHeaders().set("icy-br","128");
-        httpExchange.getResponseHeaders().set("icy-pub","1");
-        httpExchange.getResponseHeaders().set("icy-description","Flux 70er");
-        httpExchange.getResponseHeaders().set("icy-audio-info","channels=2;samplerate=44100;bitrate=128");
+        setHeaders(httpExchange);
         httpExchange.sendResponseHeaders(200, 0);
         try {
             Thread.sleep(10000);
-        }catch (Exception e){}
+
+        } catch (Exception e) {
+        }
         new Thread(()-> {
             try {
-                outputStream.write(IOUtils.toByteArray(new BufferedInputStream(this.client.inputStream)));
-            } catch (IOException e) {}
-        }).start();
-        while (true){
-            System.out.println(new BufferedReader(new InputStreamReader(this.client.inputStream)).readLine());
-
-
-//            outputStream.write(IOUtils.toByteArray(client.inputStream));
-            if (client.inputStream == null){
-                System.out.println("null");
-            }else{
-                System.out.println("not null");
+                client.getInputStream().transferTo(outputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            //outputStream.write(client.buf);
-            //this.client.inputStream.transferTo(outputStream);
-            outputStream.flush();
+        }).start();
+
+        outputStream.flush();
+
+
+
+    }
+    private void setHeaders (HttpExchange httpExchange){
+        Map<String, String> headers = client.getResponseHeaders();
+        for (Map.Entry<String, String> entry : headers.entrySet()){
+            httpExchange.getResponseHeaders().set(entry.getValue(), entry.getKey());
         }
-        //outputStream.close();
     }
 }
